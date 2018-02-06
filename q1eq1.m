8e25f76a523501b868cb1d092dce403fa5f14c6c
TomackG <36164841+TomackG@users.noreply.github.com>
2018-02-06 09:46:53 +0000

    Delete q1eq1.m

--- q1eq1.m
+++
@@ -1,7 +0,0 @@
-function xdot = q1eq1( t,x )
-g=9.80665;% This is the gravitational acceleration.
-xdot(1)=x(2);%This calculates F_1 in terms of initial conditions.
-xdot(2)=-g*sin(x(1));%This calculates F_2 in terms of initial conditions.
-xdot=xdot';%This returns a vector.
-end
-
